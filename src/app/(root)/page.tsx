7b63ab5dceadb1f1c184517aa148c607d73d770d
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-03-14 12:44:05 +0100

    Add HeaderBox

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
@@ -15,7 +16,10 @@ const Home = () => {
             user = {loggedIn?.firstName || 'Guest'}
             subtext = "Access and manage your account and transactions efficiently."
           />
+
+          <TotalBalanceBox
           
+          />
         </header>
       </div>
     </section>

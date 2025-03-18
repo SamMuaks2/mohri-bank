55ab27f7ed97379d35a0fa15dabf230b6b4c2a9d
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-03-18 10:47:20 +0100

    Add sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,17 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Mohri", lastName: "Muakpo" };
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+        {/* SIDEBAR */}
         {children}
     </main>
   );
